8f3ca1f737aa3f311c5bae65aff9e43aa6f5c5bc
jessabelle <jessabelle.delen.ramos@gmail.com>
2018-04-30 11:07:59 -0700

    Updated AdventureFrame
    One long press = one short press, fixed the multiple-step problem :)

--- AdventureFrame.java
+++ AdventureFrame.java
@@ -1,71 +1,90 @@
 import javax.swing.JFrame;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+
 /**
  * 
- *  Describes the JFrame the Adventure game should open in
- *  Also controls the game // TODO
+ * Describes the JFrame the Adventure game should open in Also controls the game
+ * // TODO
  *
- *  @author  
- *  @version Apr 27, 2018
- *  @author  Period: 4
- *  @author  Assignment: Adventure
+ * @author
+ * @version Apr 27, 2018
+ * @author Period: 4
+ * @author Assignment: Adventure
  *
- *  @author  Sources: 
+ * @author Sources:
  */
 public class AdventureFrame extends JFrame
 {
     private AdventureComponent scene;
+
     private static final int FRAME_WIDTH = 800;
+
     private static final int FRAME_HEIGHT = 500;
+
     private PlayerMovementKeyListener keyListener;
-    
+
+
     class PlayerMovementKeyListener implements KeyListener
     {
+        private boolean pressed;
+
+
         public void keyPressed( KeyEvent k )
         {
-            
-            
+            if ( !pressed )
+            {
+                int keyCode = k.getKeyChar();
+                if ( keyCode == 97 )
+                {
+                    // A
+                    scene.movePlayer( Move.LEFT );
+                }
+                else if ( keyCode == 119 )
+                {
+                    // W
+                    scene.movePlayer( Move.UP );
+                }
+                else if ( keyCode == 100 )
+                {
+                    // D
+                    scene.movePlayer( Move.RIGHT );
+                }
+                else if ( keyCode == 115 )
+                {
+                    // S
+                    scene.movePlayer( Move.DOWN );
+                }
+                pressed = true;
+            }
+
         }
+
 
         public void keyReleased( KeyEvent k )
         {
-            
-            
+            pressed = false;
         }
+
 
         public void keyTyped( KeyEvent k )
         {
-            int keyCode = k.getKeyChar();
-            if (keyCode == 97) {
-                // A
-                scene.movePlayer(Move.LEFT);
-            }
-            else if (keyCode == 119) {
-                // W
-                scene.movePlayer(Move.UP);
-            }
-            else if (keyCode == 100) {
-                // D
-                scene.movePlayer(Move.RIGHT);
-            }
-            else if (keyCode == 115) {
-                // S
-                scene.movePlayer(Move.DOWN);
-            }
+      
         }
     }
-  
-    
+
+
     /**
      * Sets parameters of the JFrame and adds the Adventure game to it
      */
-    public AdventureFrame() {
+    public AdventureFrame()
+    {
         scene = new AdventureComponent();
-        add(scene);
-        setSize( FRAME_WIDTH, FRAME_HEIGHT);
+        add( scene );
+        setSize( FRAME_WIDTH, FRAME_HEIGHT );
         keyListener = new PlayerMovementKeyListener();
-        scene.addKeyListener(keyListener);
+        scene.addKeyListener( keyListener );
         scene.setFocusable( true );
     }
 }
