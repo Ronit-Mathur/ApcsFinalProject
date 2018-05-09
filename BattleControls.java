f04cd548618a980b10fc9386db2c75f016fc2c1a
jessabelle <jessabelle.delen.ramos@gmail.com>
2018-05-09 14:06:55 -0700

    Fixed the extra monster attack after battle ends; added [final] cast to Battle in BattleControls bc eclipse said so :moon:

--- BattleControls.java
+++ BattleControls.java
@@ -8,49 +8,56 @@ import java.awt.event.ActionListener;
 
 import java.awt.GridLayout;
 
+
 public class BattleControls extends JPanel
 {
-	private JLabel label = new JLabel("Fight!");
+    private JLabel label = new JLabel( "Fight!" );
 
-	private ActionListener attack;
-	private ActionListener block;
+    private ActionListener attack;
 
-	public BattleControls(Battle battle) 
-	{
-		JButton aButton = new JButton("Attack");
-		JButton bButton = new JButton("Block");
-		attack = new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				if ( !battle.checkEnded() )
-				{
-					battle.playerAttack();
-					System.out.println("Attacked");
-					battle.monsterAttack();
-				}
-				else {
-					System.out.println("Not Attacked");
-					// TODO: close window
-				}
-				 //TODO
-			}
-		};
-		aButton.addActionListener(attack);
-		block = new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				System.out.println("Blocked"); //TODO
-			}
-		};
-		bButton.addActionListener(block);
-		add(aButton);
-		this.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		this.setLayout(new GridLayout(0, 1));
-		this.add(aButton);
-		this.add(bButton);
-		this.add(label);
-	}
+    private ActionListener block;
 
+
+    public BattleControls( final Battle battle )
+    {
+        JButton aButton = new JButton( "Attack" );
+        JButton bButton = new JButton( "Block" );
+        attack = new ActionListener()
+        {
+            public void actionPerformed( ActionEvent e )
+            {
+                if ( !battle.checkEnded() )
+                {
+                    battle.playerAttack();
+                    System.out.println( "Attacked" );
+                    if ( !battle.checkEnded() )
+                    {
+                        battle.monsterAttack();
+                    }
+                }
+                else
+                {
+                    System.out.println( "Not Attacked" );
+                    // TODO: close window
+                }
+                // TODO
+            }
+        };
+        aButton.addActionListener( attack );
+        block = new ActionListener()
+        {
+            public void actionPerformed( ActionEvent e )
+            {
+                System.out.println( "Blocked" ); // TODO
+            }
+        };
+        bButton.addActionListener( block );
+        add( aButton );
+        this.setBorder( BorderFactory.createEmptyBorder( 30, 30, 10, 30 ) );
+        this.setLayout( new GridLayout( 0, 1 ) );
+        this.add( aButton );
+        this.add( bButton );
+        this.add( label );
+    }
 
 }
