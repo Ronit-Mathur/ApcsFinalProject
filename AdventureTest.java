16645e47dd74c6b1477c9a55195167e4eb630b42
Ronstergreen <38735296+ronstergreen@users.noreply.github.com>
2018-05-16 12:43:39 -0700

    Continued Player Testing

--- AdventureTest.java
+++ AdventureTest.java
@@ -4,14 +4,54 @@ import org.junit.Test;
 
 public class AdventureTest {
 
+	WorldMap w = new WorldMap(31, 31);
 	@Test
 	public void playerInitialization() 
 	{
-		Player player = new Player(10, 10, null, 10, 100, 10);
+		Player player = new Player(10, 10, w, 10, 100, 10);
 		assertEquals(player.getHealth(), 100);
 	}
 	
-	public void 
+	@Test
+	public void checkIfMoveUp()
+	{
+		Player player = new Player(10, 10, w, 10, 100, 10);
+		int rTest = player.getPlayerRow();
+		player.moveUp();
+		assertEquals( rTest, player.getPlayerRow() + 1);
+	}
 
+	@Test
+	public void checkIfMoveDown()
+	{
+		Player player = new Player(10, 10, w, 10, 100, 10);
+		int rTest = player.getPlayerRow();
+		player.moveDown();
+		assertEquals( rTest, player.getPlayerRow() - 1);
+	}
+	
+	@Test
+	public void checkIfMoveLeft()
+	{
+		Player player = new Player(10, 10, w, 10, 100, 10);
+		int rTest = player.getPlayerRow();
+		player.moveLeft();
+		assertEquals( rTest, player.getPlayerCol() + 1);
+	}
+	
+	@Test
+	public void checkIfMoveRight()
+	{
+		Player player = new Player(10, 10, w, 10, 100, 10);
+		int rTest = player.getPlayerRow();
+		player.moveRight();
+		assertEquals( rTest, player.getPlayerCol() - 1);
+	}
+	
+	public void checkJump()
+	{
+		Player player = new Player(10, 10, 2, 10, 100, 10);
+		
+	}
 }
 	
