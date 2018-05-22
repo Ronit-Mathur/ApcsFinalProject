5e53f05c9f59707a2343e43e72fe3e7fbb92dbb0
jessabelle <jessabelle.delen.ramos@gmail.com>
2018-05-22 11:43:43 -0700

    Lots of Tests :steam_locomotive:

--- gPeck.java
+++ gPeck.java
@@ -3,13 +3,19 @@ public class gPeck extends Monster
 {
     private static WorldMap world;
 
+    /**
+     * gPeck is a modified Monster
+     * 
+     */
     public gPeck()
     {
 
         super( 10, 300);
 
     }
-
+    /**
+     * @return monster's name (gPeck)
+     */
     public String getName() 
     {
         return "G Peck";
