2a4fd2254bdc2661689f7dc68b03433b56caacd8
Shannon Ding <shannon_ding@yahoo.com>
2018-05-29 17:26:08 -0700

    world map tests

--- WorldMapTests.java
+++ WorldMapTests.java
@@ -15,6 +15,10 @@ public class WorldMapTests
     @Test
     public void showAroundTests()
     {
+        WorldMap m = new WorldMap( 10, 10 );
+        Player p = new Player(0, 0, m);
+        m.showAround( p );
+        
         // TODO regular showAround
         // TODO showAroundRandom
     }
