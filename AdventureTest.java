d689cbb801077bb980b547bc6f1a8198c93fc206
ronstergreen <38735296+ronstergreen@users.noreply.github.com>
2018-05-29 17:31:15 -0700

    Merged tests

--- AdventureTest.java
+++ AdventureTest.java
@@ -10,6 +10,12 @@ public class AdventureTest
 
     WorldMap w = new WorldMap( 31, 31 );
     // AdventureComponent Tests
+    
+    Player player = new Player( 10, 10, w, 10, 100, 10 );
+
+    Monster monster = new gPeck();
+    
+    Monster mon = new Monster( 10, 100 );
 
 
     @Test
@@ -91,15 +97,402 @@ public class AdventureTest
 
     // Monster and Monster subclasses Tests
 
-    // TODO
-    // Move.java ?
 
     // TODO
     // Square.java
 
 
     // WorldMap.java Tests
+    
+    
+    //BATTLE TESTS
+    
+    @Test
+    public void battleConstructor()
+    {
+        Battle b = new Battle( player, monster );
+        Battle b2 = new Battle( player );
+        assertNotNull( b );
+        assertNotNull( b2 );
+    }
 
-    // TODO AdventureFrame
-    // TODO AdventureComponent ?
+
+    @Test
+    public void startBattleTest()
+    {
+        Battle b = new Battle( player, monster );
+        b.startBattle();
+        assertFalse( b.checkEnded() );
+    }
+
+
+    // TODO
+    // @Test
+    // public void checkEndedTest()
+    // {
+    // Battle b = new Battle( player, monster );
+    // b.startBattle();
+    // b.monsterAttack();
+    // assertTrue( b.checkEnded() );
+    // }
+
+    @Test
+    public void endBattleTest()
+    {
+        Battle b = new Battle( player, monster );
+        b.startBattle();
+        b.monsterAttack();
+        b.endBattle( false );
+    }
+
+
+    @Test
+    public void monsterAttackTest()
+    {
+        Battle b = new Battle( player, monster );
+        b.monsterAttack();
+        assertTrue( ( player.getHealth() > ( 100 - 15 ) ) || ( player.getHealth() < ( 100 - 5 ) ) );
+    }
+
+
+    @Test
+    public void playerAttackTest()
+    {
+        Battle b = new Battle( player, monster );
+        b.playerAttack();
+        assertTrue(
+            ( monster.getHealth() > ( 300 - 15 ) ) || ( monster.getHealth() < ( 300 - 5 ) ) );
+    }
+
+    // actionperformed test ????
+    // TODO
+    
+    
+    //MONSTER TESTS
+    
+    @Test
+    public void monsterConstructor()
+    {
+
+        Monster mon2 = new Monster();
+        assertNotNull( mon );
+        assertNotNull( mon2 );
+    }
+
+
+    @Test
+    public void getMonsterDamage()
+    {
+        int d = mon.getDamage();
+        assertTrue( ( d > ( 100 - 15 ) ) || ( d < ( 100 - 5 ) ) );
+    }
+
+
+    @Test
+    public void getMonsterHealth()
+    {
+        assertEquals( 100, mon.getHealth() );
+    }
+
+
+    @Test
+    public void monsterLowerHealth()
+    {
+        // monster starting health 100
+        mon.lowerHealth( 10 );
+        assertEquals( 90, mon.getHealth() );
+    }
+
+
+    @Test
+    public void getMonsMaxHealth()
+    {
+        mon.lowerHealth( 10 );
+        assertEquals( mon.getHealth() + 10, mon.getMaxHealth() );
+    }
+
+
+    @Test
+    public void getMonsName()
+    {
+        assertEquals( "Monster", mon.getName() );
+    }
+
+
+    // gPeck
+    /**
+     * Tests constructor and getName() for gPeck
+     */
+    @Test
+    public void gPeckTests()
+    {
+        Monster g = new gPeck();
+        assertNotNull( g );
+        assertEquals( "G Peck", g.getName() );
+    }
+
+
+    /**
+     * Tests constructor and getName() for papaPeck
+     */
+    @Test
+    public void papaPeckTests()
+    {
+        Monster p = new papaPeck();
+        assertEquals( "Papa Peck", p.getName() );
+        assertNotNull( p );
+    }
+
+
+    /**
+     * tests constructor and getName() for lilPeck
+     */
+    @Test
+    public void lilTests()
+    {
+        Monster lil = new lilPeck();
+        assertNotNull( lil );
+        assertEquals( "lil' Peck", lil.getName() );
+
+    }
+
+
+    /**
+     * tests constructor and getName() for Shannonster
+     */
+    @Test
+    public void ShannonsterConstructor()
+    {
+        Monster sh = new Shannonster();
+        assertNotNull( sh );
+        assertEquals( "Shannonster", sh.getName() );
+    }
+    
+    
+    //PLAYER TESTS
+    
+    @Test
+    public void playerInitialization()
+    {
+        assertEquals( player.getHealth(), 100 );
+
+        assertNotNull( player );
+        Player player2 = new Player( 10, 10, w );
+        assertNotNull( player2 );
+    }
+
+
+    @Test
+    public void checkIfMoveUp()
+    {
+        int rTest = player.getPlayerRow();
+        player.moveUp();
+        assertEquals( rTest, player.getPlayerRow() + 1 );
+    }
+
+
+    @Test
+    public void checkIfMoveDown()
+    {
+        int rTest = player.getPlayerRow();
+        player.moveDown();
+        assertEquals( rTest, player.getPlayerRow() - 1 );
+    }
+
+
+    @Test
+    public void checkIfMoveLeft()
+    {
+        int rTest = player.getPlayerRow();
+        player.moveLeft();
+        assertEquals( rTest, player.getPlayerCol() + 1 );
+    }
+
+
+    @Test
+    public void checkIfMoveRight()
+    {
+        int rTest = player.getPlayerRow();
+        player.moveRight();
+        assertEquals( rTest, player.getPlayerCol() - 1 );
+    }
+
+
+    @Test
+    public void checkJump()
+    {
+        // diagonal jump
+        int rTest = player.getPlayerRow();
+        int cTest = player.getPlayerCol();
+        player.jump( 11, 11 );
+        assertEquals( rTest, player.getPlayerCol() - 1 );
+        assertEquals( cTest, player.getPlayerCol() - 1 );
+        // when do we ever use jump tho
+    }
+
+
+    @Test
+    public void checkPlayerRow()
+    {
+        assertEquals( player.getPlayerRow(), 10 );
+    }
+
+
+    @Test
+    public void checkPlayerCol()
+    {
+        assertEquals( player.getPlayerCol(), 10 );
+    }
+
+
+    @Test
+    public void checkHealth()
+    {
+        player.lowerHealth( 10 );
+        int h = player.getHealth();
+        assertEquals( h, 90 );
+    }
+
+
+    @Test
+    public void checkDamage()
+    {
+        int d = player.getDamage();
+        assertTrue( d >= 5 && d <= 15 );
+
+    }
+
+
+    @Test
+    public void checkBaseDamage()
+    {
+        int d = player.getBaseDamage();
+        assertEquals( player.increaseDamage( 10 ), d + 10 );
+    }
+
+
+    @Test
+    public void checkMaxHealth()
+    {
+        player.lowerHealth( 10 );
+        int h = player.getHealth();
+        int m = player.getMaxHealth();
+        assertEquals( h + 10, m );
+
+    }
+
+
+    @Test
+    public void checkLower()
+    {
+        player.lowerHealth( 10 );
+        assertEquals( player.getHealth(), 90 );
+    }
+
+
+    @Test
+    public void checkIncrease()
+    {
+        player.lowerHealth( 50 );
+        player.increaseHealth( 10 );
+        assertEquals( player.getHealth(), 60 );
+    }
+
+
+    @Test
+    public void checkIncreaseMax()
+    {
+      
+        player.increaseMaxHealth( 10 );
+        assertEquals( player.getMaxHealth(), 110 );
+    }
+
+
+    @Test
+    public void checkIncToMax()
+    {
+        int mh = player.getMaxHealth();
+        player.lowerHealth( 10 );
+        assertEquals( mh, player.increaseHealthToMax() );
+
+    }
+
+
+    @Test
+    public void increaseDmgTest()
+    {
+        player.increaseDamage( 10 );
+        assertEquals( player.getBaseDamage(), 20 );
+    }
+
+
+    @Test
+    public void getBlockTest()
+    {
+        assertEquals( player.getBlock(), 10 );
+
+    }
+
+
+    @Test
+    public void increaseBlockTest()
+    {
+        int b = player.getBlock();
+        int inc = player.increaseBlock( 10 );
+        assertEquals( inc, b + 10 );
+    }
+
+
+    @Test
+    public void checkIfDead()
+    {
+        player.lowerHealth( 100 );
+        assertTrue( player.checkDeath() );
+    }
+
+
+    @Test
+    public void checkSteps()
+    {
+        player.moveDown();
+        assertEquals( player.getSteps(), 1 );
+    }
+
+
+    @Test
+    public void getWorld()
+    {
+        WorldMap g = player.getWorld();
+        assertEquals( w, g );
+    }
+    
+    
+    //WORLD MAP TESTS
+    
+    @Test
+    public void WorldMapTests()
+    {
+        WorldMap m = new WorldMap( 10, 10 );
+        assertNotNull( m );
+    }
+
+
+    @Test
+    public void showAroundTests()
+    {
+        WorldMap m = new WorldMap( 10, 10 );
+        Player p = new Player(0, 0, m);
+        m.showAround( p );
+        
+        // TODO regular showAround
+        // TODO showAroundRandom
+    }
+
+
+    public void getMapTest()
+    {
+        WorldMap m = new WorldMap( 10, 10 );
+        assertNotNull( m.getMap() );
+    }
+    
 }
